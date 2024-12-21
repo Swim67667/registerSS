b3b2a0f2ec1dc22e2add6acd38c4553467a7c663
Swim67667 <150874399+Swim67667@users.noreply.github.com>
2024-12-21 11:44:15 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+  <title>School Store Sign Up</title>
+    <style>
+      body{
+        background-color:navy;
+      }
+      h1{
+      color:white;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>School Store Sign Ups!</h1>     
+  </body>
+</html>
